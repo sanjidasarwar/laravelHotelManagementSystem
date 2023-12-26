6f71fc55b0d19082f1a236fab6061efa7d0d9281
tupsi07 <sanjida657@yahoo.com>
2023-12-27 00:14:04 +0600

    create user dashboard

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\UserController;
 Route::get('/', [UserController::class, 'index']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.user.user_dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
